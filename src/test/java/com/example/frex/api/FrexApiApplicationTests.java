47f80a7b6369e3de11bc9328f6b360655b9e6782
HarishTCX <kumardeepaktc@gmail.com>
2025-05-18 11:42:55 +0530

    Initial commit

---
+++ src/test/java/com/example/frex/api/FrexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
